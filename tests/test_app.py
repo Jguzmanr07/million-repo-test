0bf6852ff16ee55b70a6d081c0b4473de38914e9
Jesús Guzmán Romero <jguzman@payvalida.com>
2024-06-15 01:38:59 -0500

    cofnigure test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,23 +1,37 @@
 import pytest
+from bs4 import BeautifulSoup
 from app import app
 
 @pytest.fixture
 def client():
+    """Fixture para obtener un cliente de prueba para la aplicación."""
     with app.test_client() as client:
         yield client
 
 def test_hello(client):
+    """Prueba del endpoint principal '/'."""
     response = client.get('/')
     assert response.status_code == 200
-    assert b'Microservice in Python' in response.data
-    assert b'Maintainer: jguzman.07@icloud.com' in response.data
+    
+    # Decodificar el contenido HTML de la respuesta
+    html_content = response.data.decode('utf-8')
+    
+    # Utilizar BeautifulSoup para analizar el contenido HTML
+    soup = BeautifulSoup(html_content, 'html.parser')
+    
+    # Buscar el mantenimiento en el contenido HTML
+    maintainer_info = soup.find('strong', string='Maintainer:')
+    assert maintainer_info is not None
+    assert 'jguzman.07@icloud.com' in maintainer_info.parent.text
 
 def test_health_check(client):
+    """Prueba del endpoint '/healthz'."""
     response = client.get('/healthz')
     assert response.status_code == 200
     assert response.data == b'OK'
 
 def test_readiness_check(client):
+    """Prueba del endpoint '/readiness'."""
     response = client.get('/readiness')
     assert response.status_code == 200
-    assert response.data == b'OK'
+    assert response.data == b'OK' 
